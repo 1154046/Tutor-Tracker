78f553722ea78ee24a0a38e16122706bbaec8873
CxL <1154046@students.wits.ac.za>
2019-11-12 06:28:24 +0200

    Update post.page.ts

--- src/post/post.page.ts
+++ src/post/post.page.ts
@@ -1,57 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-import { UserService } from '../users.service';
-import { firestore } from 'firebase/app';
-// tslint:disable: indent
+// import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+// import { UserService } from '../users.service';
+// import { firestore } from 'firebase/app';
+// // tslint:disable: indent
 
-@Component({
-	selector: 'app-post',
-	templateUrl: './post.page.html',
-	styleUrls: ['./post.page.scss'],
-})
-export class PostPage implements OnInit {
+// @Component({
+// 	selector: 'app-post',
+// 	templateUrl: './post.page.html',
+// 	styleUrls: ['./post.page.scss'],
+// })
+// export class PostPage implements OnInit {
 
-  postID: string;
-	effect = '';
-	post;
-	postReference: AngularFirestoreDocument;
-	sub;
+//   postID: string;
+// 	effect = '';
+// 	post;
+// 	postReference: AngularFirestoreDocument;
+// 	sub;
 
-	heartType = 'heart-empty';
+// 	heartType = 'heart-empty';
 
-	constructor(
-		private route: ActivatedRoute,
-		private afs: AngularFirestore,
-		private user: UserService) {
+// 	constructor(
+// 		private route: ActivatedRoute,
+// 		private afs: AngularFirestore,
+// 		private user: UserService) {
 
-	}
+// 	}
 
-	ngOnInit() {
-		this.postID = this.route.snapshot.paramMap.get('id');
-		this.postReference = this.afs.doc(`posts/${this.postID}`);
-		this.sub = this.postReference.valueChanges().subscribe(val => {
-			this.post = val;
-			this.effect = val.effect;
-			this.heartType = val.likes.includes(this.user.getUID()) ? 'heart' : 'heart-empty';
-		});
-	}
+// 	ngOnInit() {
+// 		this.postID = this.route.snapshot.paramMap.get('id');
+// 		this.postReference = this.afs.doc(`posts/${this.postID}`);
+// 		this.sub = this.postReference.valueChanges().subscribe(val => {
+// 			this.post = val;
+// 			this.effect = val.effect;
+// 			this.heartType = val.likes.includes(this.user.getUID()) ? 'heart' : 'heart-empty';
+// 		});
+// 	}
 
-	// tslint:disable-next-line:use-lifecycle-interface
-	ngOnDestroy() {
-		this.sub.unsubscribe();
-	}
+// 	// tslint:disable-next-line:use-lifecycle-interface
+// 	ngOnDestroy() {
+// 		this.sub.unsubscribe();
+// 	}
 
-	toggleHeart() {
-		if (this.heartType === 'heart-empty') {
-			this.postReference.update({
-				likes: firestore.FieldValue.arrayUnion(this.user.getUID())
-			});
-		} else {
-			this.postReference.update({
-				likes: firestore.FieldValue.arrayRemove(this.user.getUID())
-			});
-		}
-	}
+// 	toggleHeart() {
+// 		if (this.heartType === 'heart-empty') {
+// 			this.postReference.update({
+// 				likes: firestore.FieldValue.arrayUnion(this.user.getUID())
+// 			});
+// 		} else {
+// 			this.postReference.update({
+// 				likes: firestore.FieldValue.arrayRemove(this.user.getUID())
+// 			});
+// 		}
+// 	}
 
-}
+// }
